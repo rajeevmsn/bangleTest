947b93efac725ddd7d09dbd8737c27fbbb83d6af
Rajeev MYLAPALLI <rajeev.msn@gmail.com>
2022-11-14 14:56:46 +0100

    created service worker (sw.js)

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'bangleTest';
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
